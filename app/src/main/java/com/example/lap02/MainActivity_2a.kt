b982f75ac203d206918c93299a4cbdaec35e4433
stoky71 <10lop96phungbuitronghieu@gmail.com>
2022-02-28 13:03:36 +0700

    LTDD tuan 2 lap02

---
+++ app/src/main/java/com/example/lap02/MainActivity_2a.kt
@@ -0,0 +1,11 @@
+package com.example.lap02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity_2a : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+    }
+}
---
+++ app/src/main/java/com/example/lap02/MainActivity_2b.kt
@@ -0,0 +1,12 @@
+package com.example.lap02
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity_2b  : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.react)
+    }
+}
